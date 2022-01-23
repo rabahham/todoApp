68c02deee22cf7c5243f53837099e1e1f4cd68ae
rabah <rabahham01@gmail.com>
2022-01-24 00:06:57 +0100

    item

--- lib/modules/new_tasks/new_tasks_screen.dart
+++ lib/modules/new_tasks/new_tasks_screen.dart
@@ -3,13 +3,37 @@ import 'package:flutter/material.dart';
 class NewTasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'New Tasks',
-        style: TextStyle(
-          fontSize: 25.0,
-          fontWeight: FontWeight.bold,
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 40.0,
+            child: Text(
+              '20:45 PM',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Titel',
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                '25 Apr 2022',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
